8dd2bcf01b35f96b25dfa67768314fd8d3a63b9b
TsNedev <tsvetannedev360@gmail.com>
2024-02-10 17:40:00 +0200

    add main layout

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,7 +4,8 @@ const router = express.Router();
 // const router = require('express').Router();
 
 router.get('/',(req,res)=>{
-    res.render(`home`,{layout : false})
+    res.render(`home`);
     });
 
 module.exports = router;
+
