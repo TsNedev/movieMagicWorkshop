e362d331c716a82a36b10866bf46880583838d6c
TsNedev <tsvetannedev360@gmail.com>
2024-02-10 18:13:55 +0200

    add 404 page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -3,6 +3,6 @@ const router = require('express').Router();
 router.get('/create',(req,res)=>{
 
   res.render('create');     
-})
+});
 
 module.exports= router; 
--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,12 @@ const router = express.Router();
 const homeController = require('./controllers/homeController');
 
 const movieController = require('./controllers/movieController');
+const errorMassageController = require('./controllers/errorMassageController'); 
 
 router.use(homeController);
 router.use(movieController);
 
+
+
+router.use(errorMassageController); 
    module.exports = router;
